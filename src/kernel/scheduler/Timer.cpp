3cded485ba3e0b642516665a0401df9acefffc59
dreamhopping <71222289+dreamhopping@users.noreply.github.com>
2021-04-07 22:22:01 +0100

    [kernel: timer] fix typo in comment

--- src/kernel/scheduler/Timer.cpp
+++ src/kernel/scheduler/Timer.cpp
@@ -7,7 +7,7 @@ int Timer::ticks;
 void Timer::initialize(int freq) {
     int divisor = 1193180 / freq;
 
-    // to set the rate at which channel 0 fires off the IRQ0, we write to the timer chanels
+    // to set the rate at which channel 0 fires off the IRQ0, we write to the timer channels
     // the timer will divide it's input clock of 1.19mhz by the number in the data register
     // to figure out how many times per second to fire that signal.
     IO::pwrite(CMD_REG_PORT, 0x36); // set our command byte, 0x36
