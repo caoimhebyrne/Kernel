64f4a13a242f72489fd3daa9d423227d7a85d05a
Conor Byrne <71222289+dreamhopping@users.noreply.github.com>
2021-04-09 08:24:42 +0100

    [kernel] print multiboot2 tag information to COM1

--- src/kernel/main.cpp
+++ src/kernel/main.cpp
@@ -27,11 +27,71 @@ extern "C" __attribute__((unused)) void kernel_main() {
                    MULTIBOOT2_BOOTLOADER_MAGIC, multiboot_magic);
         halt();
         return;
+    } else if (multiboot_ptr & 7) {
+        Display::drawString("Unaligned MBI!");
+        IO::printf("Unaligned MBI received! Address: 0x%l...\nhalting!\n",
+                   multiboot_ptr);
+        halt();
+        return;
     }
 
+    auto size = *(unsigned *) multiboot_ptr;
     IO::printf(
-            "========== Multiboot info dump ==========\n- Received magic: 0x%l\n- Received pointer: 0x%l\n=========================================\n",
-            multiboot_magic, multiboot_ptr);
+            "========== Multiboot info dump ==========\n- Received magic: 0x%l\n- Received pointer: 0x%l\n- MBI size: 0x%l\n",
+            multiboot_magic, multiboot_ptr, size);
+
+    // read multiboot2 information passed to us via the pointer
+    struct multiboot_tag *tag;
+    for (tag = (struct multiboot_tag *) (multiboot_ptr + 8);
+         tag->type != MULTIBOOT_TAG_TYPE_END;
+         tag = (struct multiboot_tag *) ((uint8_t *) tag
+                                         + ((tag->size + 7) & ~7))) {
+        switch (tag->type) {
+            case MULTIBOOT_TAG_TYPE_CMDLINE: {
+                char *string = ((struct multiboot_tag_string *) tag)->string;
+                // IO::printf("- Command line: %s\n", string);
+                break;
+            }
+            case MULTIBOOT_TAG_TYPE_BOOT_LOADER_NAME:
+                IO::printf("- Boot loader name: %s\n",
+                           ((struct multiboot_tag_string *) tag)->string);
+                break;
+            case MULTIBOOT_TAG_TYPE_BASIC_MEMINFO:
+                IO::printf("- Lower memory: %iKB\n- Upper memory: %iKB\n",
+                           ((struct multiboot_tag_basic_meminfo *) tag)->mem_lower,
+                           ((struct multiboot_tag_basic_meminfo *) tag)->mem_upper);
+                break;
+            case MULTIBOOT_TAG_TYPE_BOOTDEV:
+                IO::printf("- Boot device: 0x%l, %l, %l\n",
+                           ((struct multiboot_tag_bootdev *) tag)->biosdev,
+                           ((struct multiboot_tag_bootdev *) tag)->slice,
+                           ((struct multiboot_tag_bootdev *) tag)->part);
+                break;
+            case MULTIBOOT_TAG_TYPE_MMAP: {
+                multiboot_memory_map_t *mmap;
+
+                IO::printf("- Memory map:\n");
+
+                for (mmap = ((struct multiboot_tag_mmap *) tag)->entries;
+                     (multiboot_uint8_t *) mmap
+                     < (multiboot_uint8_t *) tag + tag->size;
+                     mmap = (multiboot_memory_map_t *)
+                             ((unsigned long) mmap
+                              + ((struct multiboot_tag_mmap *) tag)->entry_size))
+                    IO::printf("  - base address: 0x%l%l,"
+                               " length: 0x%l%l, type: 0x%l\n",
+                               (unsigned) (mmap->addr >> 32),
+                               (unsigned) (mmap->addr & 0xffffffff),
+                               (unsigned) (mmap->len >> 32),
+                               (unsigned) (mmap->len & 0xffffffff),
+                               (unsigned) mmap->type);
+            }
+                break;
+        }
+    }
+
+    IO::printf("=========================================\n");
+
 
     // loop through all global constructors and invoke them
     void (**constructor)() = &__CTOR_LIST__;
