427ee47e96da3a3cbaa8d07822e934ce50b43f41
Conor Byrne <71222289+dreamhopping@users.noreply.github.com>
2021-04-09 14:48:11 +0100

    [kernel: scheduler] import stdint.h in Task.h

--- src/kernel/scheduler/task/Task.h
+++ src/kernel/scheduler/task/Task.h
@@ -2,6 +2,7 @@
 #define KERNEL_TASK_H
 
 #include <stddef.h> // NOLINT(modernize-deprecated-headers)
+#include <stdint.h> // NOLINT(modernize-deprecated-headers)
 
 struct Task {
     size_t rax, rbx, rcx, rdx, rsi, rdi, rsp, rbp, r8, r9, r10, r11, r12, r13, r14, r15, rflags, cr0, cr2, cr3, cr4, cr8;
