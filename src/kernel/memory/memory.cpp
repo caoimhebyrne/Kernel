561427252ca4c9f22eb6f870932353f3738780cf
Conor Byrne <71222289+dreamhopping@users.noreply.github.com>
2021-04-10 22:55:12 +0100

    [kernel: memory] add memcpy
    this patch adds a simple memcpy function which will copy bytes from one destination to another!
    this commit also changes memset to use size_t instead of auto (int in this case) for i in the loop

--- src/kernel/memory/memory.cpp
+++ src/kernel/memory/memory.cpp
@@ -8,9 +8,27 @@
  */
 extern "C" [[maybe_unused]] void *memset(void *pointer, int byte, size_t count) {
     auto *castedPointer = (char *) pointer;
-    for (auto i = 0; i < count; i++) {
+    for (size_t i = 0; i < count; i++) {
         castedPointer[i] = (char) (byte & 0xff);
     }
 
     return pointer;
+}
+
+/**
+ * copies a certain amount of bytes from one location to another
+ *
+ * @param destination the area to copy bytes from
+ * @param source the area to move the bytes to
+ * @param count the amount of times to iterate this
+ */
+extern "C" [[maybe_unused]] void *memcpy(void *destination, void *source, size_t count) {
+    auto *castedDestination = (char *) destination;
+    auto *castedSource = (char *) source;
+
+    for (size_t i = 0; i < count; i++) {
+        castedDestination[i] = castedSource[i];
+    }
+
+    return destination;
 }
